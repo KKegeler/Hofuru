d77638cca9cade4e071c555398081e2019a72e5f
Martin Keren <kerenm@hochschule-trier.de>
2017-01-16 18:19:28 +0100

    git fix eventuell

---
+++ Assets/Scripts/GameController/PlayerStats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public float maxShurikenCount;
+    public float maxTeleportPower;
+
+    private float curShurikenCount;
+    private float curHealth;
+    private float curTeleportPower;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

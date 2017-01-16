808133a2b0eaf7c2c3e1766262a56e8a59126335
Martin Keren <kerenm@hochschule-trier.de>
2017-01-16 19:34:38 +0100

    weißes TeleportIcon und kleiner Bugfix beim EnemyDeath!

--- Assets/Scripts/Health/EnemyDeath.cs
+++ Assets/Scripts/Health/EnemyDeath.cs
@@ -9,6 +9,7 @@ public class EnemyDeath : Death {
     SpriteRenderer sr;
     EnemyMachine em;
     Animator animator;
+    Component[] colliders;
 
     // Use this for initialization
     void Start() {
@@ -17,11 +18,15 @@ public class EnemyDeath : Death {
         sr = this.gameObject.GetComponent<SpriteRenderer>();
         em = GetComponent<EnemyMachine>();
         animator = this.gameObject.GetComponent<Animator>();
+        this.colliders = this.gameObject.GetComponentsInChildren<Collider2D>();
     }
 
     public override void HandleDeath() {
         MessagingSystem.Instance.QueueMessage(new ScoreMessage(100));
-
+        for(int i = 0; i < colliders.Length; i++) {
+            Collider2D col = (Collider2D)colliders[i];
+            col.enabled = false;
+        }
         this.rb.bodyType = RigidbodyType2D.Static;
         this.col.enabled = false;
         //this.sr.color = new Color(0, 0, 0, 0.25f);
--- Assets/Scripts/Player/Teleport.cs
+++ Assets/Scripts/Player/Teleport.cs
@@ -45,7 +45,7 @@ public class Teleport : MonoBehaviour {
         }
         if (isActive) {
             //Vector3 moveDelta = new Vector3(Input.GetAxis("HorizontalR"), Input.GetAxis("VerticalR") * -1, 0) * Time.unscaledDeltaTime * targetSpeed;
-            if (Vector3.Distance(teleportTarget.transform.position, player.transform.position) > maxDistance) {
+            if (Vector3.Distance(teleportTarget.transform.position, player.transform.position) > maxDistance + 3) {
                 teleportTarget.GetComponent<SpriteRenderer>().color = Color.red;
             }
             else {

0b6c31cc5da7f81d418427c90ff3f5c3889b51bd
Martin Keren <kerenm@hochschule-trier.de>
2017-03-10 10:20:53 +0100

    SlideKick begonnen

---
+++ Assets/Scripts/Player/SlideKick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideKick : MonoBehaviour {
+
+    private PlayerMovement pm;
+
+	// Use this for initialization
+	void Start () {
+        this.pm = GameObjectBank.instance.player.GetComponent<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (pm.DoesSlide() && other.GetComponent<Enemy>()) {
+            Vector2 dir;
+            dir = (other.transform.position - this.transform.position);
+            dir.x *= 10;
+            dir = dir.normalized;
+            Debug.Log("dir: " + dir);
+            other.GetComponent<Rigidbody2D>().AddForce(dir * 250, ForceMode2D.Impulse);
+            /// TODO: FREEZE
+        }
+    }
+}

d5d15c38c8d40af0e9987ac4a039924d793ef3f6
hockemeh <h.hermann94@web.de>
2017-03-13 19:07:25 +0100

    + EnemyAttack jetzt wirklich hinzugefügt

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,73 @@
+﻿using Framework.Pool;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+    public float damage;
+    public float meleeForce;
+    public float minDelay;
+    public float maxDelay;
+
+    private float attackTime;
+    private bool inRange;
+
+    public GameObject bloodParticle;
+
+    private Animator animator;
+    private Rigidbody2D rbPlayer;
+    private Health hcPlayer;
+    private GameObject blood;
+
+    // Use this for initialization
+    void Start () {
+        animator = transform.parent.GetComponent<Animator>();
+        rbPlayer = GameObjectBank.Instance.player.GetComponent<Rigidbody2D>();
+        hcPlayer = GameObjectBank.Instance.player.GetComponent<Health>();
+        blood = GameObjectBank.Instance.blut;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (inRange)
+        {
+            if (attackTime > 0.0f)
+            {
+                attackTime -= Time.deltaTime;
+                return;
+            }
+            // attack animation
+            animator.SetBool("doesMelee", true);
+            // deal damage
+            Vector2 dir = Vector2.right;
+            if (transform.parent.position.x > rbPlayer.position.x)
+            {
+                dir = Vector2.left;
+            }
+            hcPlayer.ReduceHealth(damage);
+            //Instantiate(GameObjectBank.Instance.blut, other.transform.position, Quaternion.Euler(new Vector3(0,0,Random.Range(-100, 100))));
+            PoolManager.Instance.ReuseObject2(blood, rbPlayer.position, Quaternion.Euler(new Vector3(0, 0, Random.Range(-100, 101))));
+            rbPlayer.AddForce(dir * meleeForce, ForceMode2D.Impulse);
+            Instantiate(bloodParticle, rbPlayer.position, Quaternion.identity);
+            // new delay
+            attackTime = Random.Range(minDelay, maxDelay);
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.GetComponent<PlayerMovement>())
+        {
+            // its the player
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.GetComponent<PlayerMovement>())
+        {
+            // its the player
+            inRange = false;
+        }
+    }
+}

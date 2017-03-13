95a26cde49b2da8af3dd379485dd5ac75a383e59
Hermann Hockemeier <h.hermann94@web.de>
2017-03-13 09:29:23 +0100

    # HoldPosition abgeändert => Gegner kann während dieser angreift etwas mehr zurückgedrängt werden

--- Assets/Scripts/Behaviour/Bhv_HoldPosition.cs
+++ Assets/Scripts/Behaviour/Bhv_HoldPosition.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class Bhv_HoldPosition : MonoBehaviour {
 
     [HideInInspector] public Vector2 position;
+    private float alpha; // 0.05f is pretty good, position and me.position aren't that different after all
 
     private Rigidbody2D me;
 
 	// Use this for initialization
 	void Start () {
         me = GetComponent<Rigidbody2D>();
+        alpha = 0.05f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        me.MovePosition(position);
+        me.MovePosition(alpha * position + (1.0f - alpha) * me.position);
 	}
 }

43461a2121f600b381515a603218043a4095dbf1
Martin Keren <kerenm@hochschule-trier.de>
2017-03-20 10:25:22 +0100

    Bombe größer mit BlinkBlink... bitte in leveln placement überprüfen!

---
+++ Assets/BlinkBlink.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkBlink : MonoBehaviour {
+    SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+        this.sr = GetComponent<SpriteRenderer>();
+        StartCoroutine(BlinkBlinkRoutine());
+	}
+	
+    IEnumerator BlinkBlinkRoutine() {
+        while (true) {
+            sr.color = Color.red;
+            yield return new WaitForSeconds(0.3f);
+            sr.color = Color.white;
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+}
